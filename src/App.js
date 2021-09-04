a490715c89a8c503dd1e272beeb73e8c43bd9f62
SMei201900 <smei201900@gmail.com>
2021-09-03 23:43:35 -0400

    Created a new file called "Search"
    Ignore the package.json edit because nothing changed about it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import Credits from "./Credits";
+import Search from "./Search.js";
 
 export default function App() {
 	return (
 		<div className="App">
 			<div className="gradient">
 				<h1>Weather App</h1>
+				<Search />
 				<br />
 				<Credits />
 			</div>
