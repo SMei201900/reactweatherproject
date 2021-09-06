12c05566937f2d5a95ea1c3c2f1463ff801cdca0
SMei201900 <smei201900@gmail.com>
2021-09-05 22:12:56 -0400

    Imported CSS Files
    Imported CSS Files to index.js to prevent local CSS from overriding the CSS code we made.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import "./App.css";
+import "./CreditStyle.css";
+import "./ButtonStyling.css";
 
 import "bootstrap/dist/css/bootstrap.css";
 
